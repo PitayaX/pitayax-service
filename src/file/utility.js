862bb57daf59a665ef51bcb78d36e4e2c563773a
Rabbit0306 <415413233@qq.com>
2015-09-14 17:23:22 +0800

    update

--- src/file/utility.js
+++ src/file/utility.js
@@ -2,24 +2,6 @@ var qiniu = require('qiniu');
 var formidable = require('formidable');
 var fileConfig = require('../startup/fileConfig').settings;
 
-exports.shortView = function(keyName, mode, width, height){
-  // return the short view of upload picture
-  var url = qiniu.rs.makeBaseUrl(fileConfig.bucketUrl, keyName);
-  var iv = new qiniu.fop.ImageView();
-
-  // settings of short view picture
-  iv.mode = isNaN(+mode)? 0 : +mode;
-  iv.width = isNaN(+width)? 0 : +width;
-  iv.height = isNaN(+height)? 0 : +height;
-
-  // create picture url
-  url = iv.makeRequest(url);
-  var policy = new qiniu.rs.GetPolicy();
-  url = policy.makeRequest(url);
-
-  return url;
-}
-
 exports.selectKey = function (data, keyString) {
   try {
     var result = {};
